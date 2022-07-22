5a5603e63bbf312d0d16bc27b57bcb33fb8ffa11
Leandro Olivera <leandro.olivera03@gmail.com>
2022-07-22 14:11:32 -0300

    Parte 1 Ok, falta parte 2

--- Ejercicio_9/Ejercicio-9-Parte1/Ejercicio-9-Parte1/Program.cs
+++ Ejercicio_9/Ejercicio-9-Parte1/Ejercicio-9-Parte1/Program.cs
@@ -23,52 +23,57 @@ int[] miVector = new int[longVector];
 
 // Lleno el vector con datos aleatorios
 
-for (int i = 0; i < longVector; i++){
+for (int i = 0; i < longVector; i++)
+{
     miVector[i] = random.Next(0, 100);
 }
 
 // Muestro el vector
 Console.WriteLine("Los valores del vector son: ");
 Console.Write("[");
-foreach (int valor in miVector){
+foreach (int valor in miVector)
+{
     contador++;
-    if (longVector == contador){
+    if (longVector == contador)
+    {
         Console.Write($"{valor}");
-    } else {
+    }
+    else
+    {
         Console.Write($"{valor}, ");
     }
 }
 Console.WriteLine("]");
 
-//5)      Invertir el vector, de manera que el primer elemento quede al último y el útimo en el primero; el segundo en anteúltimo, el 
-//anteúltimo en el segundo y así sucesivamente. En otra palabras si el vector es de 5 posiciones y el usuario ingresó: 10, 20, 30, 40, 50, 
-//una vez invertido, el vector debe quedar así: 50, 40, 30, 20, 10.  Se debe usar solo lo visto en clase hasta ahora y no los métodos que 
-//trae C# para estas cuestiones. Tampoco se debe crear un vector nuevo o auxiliar para realizar el ejercicio. 
-//6)      Mostrar el vector nuevamente
+int auxInicial = miVector[0];
+int auxPosicion = longVector - 1;
+int auxFinal = miVector[auxPosicion];
 
-int auxInicial=miVector[0];
-int auxPosicion = longVector-1;
-int auxFinal=miVector[longVector];
 
-if (miVector.Length%2 == 0){
-    for(int i = 0; i < miVector.Length; i++){
+for (int i = 0; i < miVector.Length/2; i++)
+    {
         auxInicial = miVector[i];
         auxFinal = miVector[auxPosicion];
 
         miVector[i] = auxFinal;
-        miVector[auxPosicion]  = auxInicial;
+        miVector[auxPosicion] = auxInicial;
 
-        auxPosicion++;
-    }   
+        auxPosicion--;
 }
+
 
 Console.WriteLine("Los valores del vector son: ");
 Console.Write("[");
-foreach (int valor in miVector){
+contador = 0;
+foreach (int valor in miVector)
+{
     contador++;
-    if (longVector == contador){
+    if (longVector == contador)
+    {
         Console.Write($"{valor}");
-    } else {
+    }
+    else
+    {
         Console.Write($"{valor}, ");
     }
 }
--- Ejercicio_9/Ejercicio-9-Parte2/Program.cs
+++ Ejercicio_9/Ejercicio-9-Parte2/Program.cs
@@ -13,12 +13,49 @@ así sucesivamente hasta la última columna la cual contiene del 80 al 90
 7)    Mostrar el carton por pantalla
 */
 
-Console.WriteLine("Bienvenido, a continuación ingrese cantidad de filas y columnas de la matriz:");
+Console.WriteLine("Bienvenido al BINGO!");
 // Petición de datos (cantidad de filas y cantidad de columnas)
-Console.Write("\n   Cantidad de filas: ");
-int cantidadFilas = int.Parse(Console.ReadLine());
-Console.Write("\n   Cantidad de Columnas: ");
-int cantidadColumnas = int.Parse(Console.ReadLine());
+Console.WriteLine("Estamos generando su nuevo cartón para el juego...");
+Console.ReadKey();
+int cantidadFilas = 3;
+int cantidadColumnas = 9;
+int cantNumeros = 15;
+int cantEspacios = 12;
+Random random = new Random();
+Boolean booleano;
 
+int[] lugaresNumeros = new int[5];
+for (int i = 0; i < lugaresNumeros.Length; i++)
+{
+    do
+    {
+        lugaresNumeros[i] = random.Next(0, 8);
+    } while (!lugaresNumeros.Contains(lugaresNumeros[i]));
+    Console.WriteLine(lugaresNumeros[i]);
+}
 // Definición de la matriz.
 int[,] numeros = new int[cantidadFilas, cantidadColumnas];
+Console.WriteLine("Los valores del vector son: ");
+for (int i = 0; i < cantidadFilas; i++)
+{
+    Console.Write("[");
+    if (i == 0)
+    {
+        Console.Write(random.Next(1, 2));
+    }
+    for (int j = 0; j < cantidadColumnas; j++)
+    {
+        numeros[i, j] = 0;
+        //Console.Write($"{numeros[i,j]} ");
+        if (numeros.GetUpperBound(1) == j)
+        {
+            Console.Write($"{numeros[i, j]}");
+        }
+        else
+        {
+            Console.Write($"{numeros[i,j]}, ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
